259c727d70427558cda36f0dc71cb3c1466f5787
Nazarko12 <nazartsiupryk@gmail.com>
2021-09-29 09:03:52 +0200

    The catalog in the form of the menu is created

--- lib/main.dart
+++ lib/main.dart
@@ -1,31 +1,44 @@
-import 'package:english_words/english_words.dart';
 import 'package:flutter/material.dart';
+import 'package:english_words/english_words.dart';
+
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Startup Name Generator',
-      theme: ThemeData(
-        primaryColor: Colors.white,
-      ),
       home: RandomWords(),
     );
   }
 }
 
+class RandomWords extends StatefulWidget {
+  @override
+  _RandomWordsState createState() => _RandomWordsState();
+}
+
 class _RandomWordsState extends State<RandomWords> {
   final _suggestions = <WordPair>[];
-  final _saved = <WordPair>{};
-  final _biggerFont = const TextStyle(fontSize: 18.0);
+  final _biggerFont = const TextStyle(fontSize: 18);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Startup Name Generator'),
+      ),
+      body: _buildSuggestions(),
+    );
+  }
+
   Widget _buildSuggestions() {
     return ListView.builder(
         padding: const EdgeInsets.all(16.0),
         itemBuilder: (context, i) {
           if (i.isOdd) return const Divider();
-
           final index = i ~/ 2;
           if (index >= _suggestions.length) {
             _suggestions.addAll(generateWordPairs().take(10));
@@ -35,107 +48,35 @@ class _RandomWordsState extends State<RandomWords> {
   }
 
   Widget _buildRow(WordPair pair) {
-    final alreadySaved = _saved.contains(pair);
     return ListTile(
       title: Text(
         pair.asPascalCase,
         style: _biggerFont,
       ),
-      trailing: Icon(
-        alreadySaved ? Icons.done : Icons.attach_money,
-        color: alreadySaved ? null : Colors.green,
-        semanticLabel: alreadySaved ? 'Remove from saved' : 'Save',
-      ),
+      trailing: const Text('Marked in the list!'),
       onTap: () {
-        setState(() {
-          if (alreadySaved) {
-            _saved.remove(pair);
-          } else {
-            _saved.add(pair);
-          }
-        });
+        _showInformation(context);
       },
     );
   }
+}
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Startup Name Generator'),
-        actions: [
-          IconButton(
-            icon: const Icon(Icons.shopping_basket),
-            onPressed: _pushSaved,
-            tooltip: 'Saved Suggestions',
+void _showInformation(BuildContext context) {
+  showDialog(
+    context: context,
+    builder: (BuildContext context) {
+      return AlertDialog(
+        title: const Text("Notification!"),
+        content: const Text("Added to catalog"),
+        actions: <Widget>[
+          FlatButton(
+            child: const Text("Okey"),
+            onPressed: () {
+              Navigator.of(context).pop();
+            },
           ),
         ],
-      ),
-      body: _buildSuggestions(),
-    );
-  }
-
-  void _pushSaved() {
-    Navigator.of(context).push(
-      MaterialPageRoute<void>(
-        builder: (context) {
-          final tiles = _saved.map(
-            (pair) {
-              return ListTile(
-                title: Text(
-                  pair.asPascalCase,
-                  style: _biggerFont,
-                ),
-              );
-            },
-          );
-          final divided = tiles.isNotEmpty
-              ? ListTile.divideTiles(
-                  context: context,
-                  tiles: tiles,
-                ).toList()
-              : <Widget>[];
-
-          return Scaffold(
-            appBar: AppBar(
-              title: const Text('Saved Suggestions'),
-              actions: [
-                IconButton(
-                  icon: const Icon(Icons.info),
-                  onPressed: () {
-                    Navigator.push(context,
-                        MaterialPageRoute<void>(builder: (context) => Info()));
-                  },
-                  tooltip: 'Saved Suggestions',
-                ),
-              ],
-            ),
-            body: ListView(children: divided),
-          );
-        },
-      ),
-    );
-  }
-}
-
-class Info extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text("Info"),
-      ),
-      body: const Center(
-          child: Text(
-        "Here you can add your words to the cart. \nYes!!! You can buy them. \n The future is now, wake up",
-        style: TextStyle(
-            fontSize: 14, color: Colors.black, fontWeight: FontWeight.normal),
-      )),
-    );
-  }
-}
-
-class RandomWords extends StatefulWidget {
-  @override
-  State<RandomWords> createState() => _RandomWordsState();
+      );
+    },
+  );
 }
